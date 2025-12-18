690642bab61d6c3d93ab809c85f984419eff4a02
Steven Exil <117109826+stexil@users.noreply.github.com>
2025-12-17 19:26:27 -0500

    Initial Expo + Firebase setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,5 @@
 import { Image } from 'expo-image';
 import { Platform, StyleSheet } from 'react-native';
-
 import { HelloWave } from '@/components/hello-wave';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
